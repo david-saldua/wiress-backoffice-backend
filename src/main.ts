c371a6f91b5ce06e4960eb408418bc6eb971e7f8
David <davidsaldua24@gmail.com>
2024-04-14 14:50:26 +0800

    chore: add unified response

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { ResponseInterceptor } from './interceptors/response.interceptor';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3001);
 }
 
